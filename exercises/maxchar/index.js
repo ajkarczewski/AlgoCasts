d528d237109fbdd46e8bb81ad5b4e6f65ae39910
ajkarczewski <karczewskiamanda@gmail.com>
2022-10-03 20:42:54 -0500

    solve maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const charMap = {}
+    let max = 0
+    let maxChar = ''
 
+    for (let char of str) {
+        if (charMap[char]) {
+            charMap[char]++
+        } else {
+            charMap[char] = 1
+        }
+    }
+    for (let char in charMap) {
+        if (charMap[char] > max) {
+            max = charMap[char]
+            maxChar = char
+        }
+    }
+    return maxChar
+}
 module.exports = maxChar;
+
+// convert string into an object
+// keys of the object are the chars in the string
+// values are the number of times the chars appear
+// THEN
+// go through the object and find the property with the highest number assigned to it
